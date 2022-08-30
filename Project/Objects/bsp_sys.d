2f7cab595ea203d5922a773a2924ccbfd1b22951
Sandman <zhangbaoning@aeonmed01.local>
2022-08-30 19:26:30 +0800

    change listings & objects to Program folder

---
+++ Project/Objects/bsp_sys.d
@@ -0,0 +1,42 @@
+.\objects\bsp_sys.o: ..\DRV\bsp_sys.c
+.\objects\bsp_sys.o: ..\DRV\bsp_sys.h
+.\objects\bsp_sys.o: ..\Libraries\CMSIS\stm32f10x.h
+.\objects\bsp_sys.o: ..\Libraries\CMSIS\core_cm3.h
+.\objects\bsp_sys.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\bsp_sys.o: ..\Libraries\CMSIS\system_stm32f10x.h
+.\objects\bsp_sys.o: ..\User\stm32f10x_conf.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_adc.h
+.\objects\bsp_sys.o: ..\Libraries\CMSIS\stm32f10x.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_bkp.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_cec.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_crc.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_dac.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_dbgmcu.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_dma.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_exti.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_flash.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_fsmc.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_gpio.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_i2c.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_iwdg.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_pwr.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_rcc.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_rtc.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_sdio.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_spi.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_tim.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_usart.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\stm32f10x_wwdg.h
+.\objects\bsp_sys.o: ..\Libraries\FWlib\inc\misc.h
+.\objects\bsp_sys.o: ..\DRV\delay.h
+.\objects\bsp_sys.o: ..\DRV\bsp_GPIO.h
+.\objects\bsp_sys.o: ..\DRV\bsp_nvic.h
+.\objects\bsp_sys.o: ..\DRV\bsp_usart.h
+.\objects\bsp_sys.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\bsp_sys.o: ..\DRV\bsp_timer.h
+.\objects\bsp_sys.o: ..\DRV\bsp_iic.h
+.\objects\bsp_sys.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\inttypes.h
+.\objects\bsp_sys.o: ..\HARDWARE\OLED\oled.h
+.\objects\bsp_sys.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\bsp_sys.o: ..\HARDWARE\MOTO\moto.h
+.\objects\bsp_sys.o: ..\HARDWARE\PID\pid.h
---
+++ Project/Objects/car.d
@@ -0,0 +1,43 @@
+.\objects\car.o: ..\HARDWARE\MOTO\car.c
+.\objects\car.o: ..\HARDWARE\MOTO\car.h
+.\objects\car.o: ..\Libraries\CMSIS\stm32f10x.h
+.\objects\car.o: ..\Libraries\CMSIS\core_cm3.h
+.\objects\car.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\car.o: ..\Libraries\CMSIS\system_stm32f10x.h
+.\objects\car.o: ..\User\stm32f10x_conf.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_adc.h
+.\objects\car.o: ..\Libraries\CMSIS\stm32f10x.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_bkp.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_cec.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_crc.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_dac.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_dbgmcu.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_dma.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_exti.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_flash.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_fsmc.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_gpio.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_i2c.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_iwdg.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_pwr.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_rcc.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_rtc.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_sdio.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_spi.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_tim.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_usart.h
+.\objects\car.o: ..\Libraries\FWlib\inc\stm32f10x_wwdg.h
+.\objects\car.o: ..\Libraries\FWlib\inc\misc.h
+.\objects\car.o: ..\DRV\bsp_sys.h
+.\objects\car.o: ..\DRV\delay.h
+.\objects\car.o: ..\DRV\bsp_GPIO.h
+.\objects\car.o: ..\DRV\bsp_nvic.h
+.\objects\car.o: ..\DRV\bsp_usart.h
+.\objects\car.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\car.o: ..\DRV\bsp_timer.h
+.\objects\car.o: ..\DRV\bsp_iic.h
+.\objects\car.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\inttypes.h
+.\objects\car.o: ..\HARDWARE\OLED\oled.h
+.\objects\car.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\car.o: ..\HARDWARE\MOTO\moto.h
+.\objects\car.o: ..\HARDWARE\PID\pid.h
